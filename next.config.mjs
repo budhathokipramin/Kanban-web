e38eaf77b0f597a5a31c8efced6247ae34ac01e3
PraminBudhathoki <budhathokipramin@gmail.com>
2024-08-27 13:56:01 +0000

    chore: Installed eslint dependencies and configured tsconfig.json, next.config.mjs, eslint.config.ts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.com/channels/@me",
+        permanent: true,
+      },
+    ];
+  }
+}
 
-export default nextConfig;
+export default nextConfig
